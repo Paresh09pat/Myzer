09f06af3e2c5a06b7796ee15803d1a089f73baa8
Paresh <09patilparesh@gmail.com>
2023-04-04 16:20:05 +0530

    changes

--- src/components/Comp1.jsx
+++ src/components/Comp1.jsx
@@ -1,20 +1,43 @@
-import React, { useState } from 'react'
+
+import React, { useRef, useState } from 'react'
 
 function Comp1() {
+    
+    const [number, setNumber] = useState(["", "", "", ""])
+    const inputOtp = useRef([])
 
-    const [number, setNumber] = useState("")
-
-    const handleInputChange = (event) => {
-        setNumber({ number: event.target.value });
+    const InputChange = (i, e) => {
+        const value = e.target.value;
+        const newOtp = [...number];
+        newOtp[i] = value;
+        setOtp(newOtp);
+        if (i < number.length - 1 && value) {
+            inputOtp.current[i + 1].focus();
+        }
     };
 
-    function handleBtnClick(e) {
-        e.preventDefault()
-        { number }
-        console.log(number)
+    const handleInput = (e) => {
+        e.preventDefault();
+        const pasteOtp = e.clipboardData.getData('text/plain').slice(0, 4);
+        const newOtp = [...number];
+        for (let i = 0; i < pasteOtp.length; i++) {
+            newOtp[i] = pasteOtp[i];
+            if (i < number.length - 1) {
+                inputOtp.current[i + 1].focus();
+            }
+        }
+        setNumber(newOtp);
+    };
+
+    const submitData = (e) => {
+        e.preventDefault();
+        console.log(number.join(''));
     }
 
-
+    const removeData = (e) => {
+        e.preventDefault();
+        setNumber(['', '', '', ''])
+    }
     return (
         <>
             {/* section one */}
@@ -26,18 +49,27 @@ function Comp1() {
                 <div className='smsblock'> <span className='otp'>SMS OTP</span> <span className='senton'>Sent on: 77777-77777 </span> </div>
 
                 {/* Input fields */}
-                <div className='inputdiv'>
-                    <input type='text' onChange={handleInputChange} className='inputfield'></input>
-                    <input type='text' onChange={handleInputChange} className='inputfield'></input>
-                    <input type='text' onChange={handleInputChange} className='inputfield'></input>
-                    <input type='text' onChange={handleInputChange} className='inputfield'></input>
-                </div>
+                <form onSubmit={submitData} >
+                    <div className='inputdiv'>
+                        {number.map((value, inx) => (
+                            <input
+                                key={inx}
+                                type="number"
+                                min="0"
+                                max="9"
+                                value={value}
+                                onChange={(event) => InputChange(inx, event)}
+                                onPaste={handleInput}
+                                ref={(e) => (inputOtp.current[inx] = e)}
+                                className='inputfield'
+                            />
+                        ))}
+                    </div>
 
-                <div className='Enterdiv'>
-                    <span className='wrong'>Entered Wrong Details? </span><span className='reenter'>Re-enter</span>
-                </div>
+                    <div className='Enterdiv'><span className='wrong'>Entered Wrong Details?</span><span className='reenter' onClick={removeData}>Re-enter</span></div>
 
-                <button onClick={handleBtnClick} className='btn'>Verify</button>
+                    <input type='submit' value="Verify" className='btn' />
+                </form>
 
                 <div className='goback'> Go back to Home</div>
             </div>
